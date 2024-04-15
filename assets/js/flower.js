d66023b5ebd2dc6bf6182e51ba1892cf9fe6f6f7
Dog Icing <77659832+DogIcing@users.noreply.github.com>
2024-04-15 13:33:12 +1000

    Add assets folder

---
+++ assets/js/flower.js
@@ -0,0 +1,19 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+var flower = new XMLHttpRequest();
+flower.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       var flowerdata = JSON.parse(this.responseText)
+       console.log(flowerdata);
+       document.getElementById("title").innerHTML = flowerdata[id].title;
+       document.getElementById("card-title").innerHTML = flowerdata[id].card_title;
+       document.getElementById("image").src = flowerdata[id].image_url;
+       document.getElementById("colour").innerHTML = flowerdata[id].colour;
+       document.getElementById("type").innerHTML = flowerdata[id].type;
+       document.getElementById("found-in").innerHTML = flowerdata[id].found_in;
+       document.getElementById("sells-for").innerHTML = flowerdata[id].price;
+       document.getElementById("description").innerHTML = flowerdata[id].description;
+    }
+};
+flower.open("GET", "/data/flowers.json", true);
+flower.send();
---
+++ assets/js/site.js
@@ -0,0 +1,8 @@
+var navbar = new XMLHttpRequest();
+navbar.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       document.getElementsByTagName("nav")[0].innerHTML = navbar.responseText;
+    }
+};
+navbar.open("GET", "/assets/navbar.html", true);
+navbar.send();
