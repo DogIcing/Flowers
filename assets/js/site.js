98a0079e53724bf65c90a656d0af68d13b20c242
Dog Icing <77659832+DogIcing@users.noreply.github.com>
2024-04-19 11:46:24 +1000

    Update site.js
    probably going to have an error lol

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,3 +6,56 @@ navbar.onreadystatechange = function() {
 };
 navbar.open("GET", "/Flowers/assets/navbar.html", true);
 navbar.send();
+
+var navdata = new XMLHttpRequest();
+navdata.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       var categories = JSON.parse(this.responseText)
+       for (let i = 0; i < categories.length; i++) {
+         var li = document.createElement("li");
+         li.classList.add("nav-item");
+         if (categories[i].dropdown = true) {
+           li.classList.add("dropdown");
+
+           //title
+           var a = document.createElement("a");
+           a.classList.add("nav-link");
+           a.classList.add("dropdown-toggle");
+           a.setAttribute("href","#");
+           a.setAttribute("role","button");
+           a.setAttribute("data-bs-toggle","dropdown");
+           a.innerHTML = categories[i].name;
+
+           //dropdown list
+           var ul = document.createElement("ul");
+           ul.classList.add("dropdown-menu");
+
+           for (let ii = 0; ii < categories[i].subcategories.length; ii++) {
+             var subcategoryheaderli = document.createElement("li");
+             var subcategoryheaderh6 = document.createElement("h6");
+             subcategoryheaderh6.innerHTML = categories[i].subcategories[ii].name;
+             subcategoryheaderli.appendChild(subcategoryheaderh6);
+             ul.appendChild(subcategoryheaderli);
+
+             for (let iii = 0; iii < categories[i].subcategories[ii].values.length; iii++) {
+               var dropdownitem = document.createElement("li");
+               var link = document.createElement("a");
+               link.innerHTML = categories[i].subcategories[ii].values[iii].name;
+               link.setAttribute("href", "/Flowers/" + categories[i].subcategories[ii].values[iii].link);
+               link.classList.add("dropdown-item");
+               dropdownItem.appendChild(link);
+               ul.appendChild(dropdownItem);
+             }
+
+             var dividerli = document.createElement("li");
+             var divider = document.createElement("hr");
+             divider.classList.add("dropdown-divider");
+             dividerli.appendChild(divider);
+             ul.appendChild(dividerli);
+           }
+         }
+       }
+    }
+};
+navdata.open("GET", "/Flowers/data/navbar.json", true);
+navdata.send();
